9332ba09eac0f00bcf73eda875d9b0317b6435c4
priyanka damodar <priyadamodar12@gmail.com>
2021-03-23 01:53:06 -0400

    wrote down the questions according to user's criteria

---
+++ Develop/index.js
@@ -0,0 +1,89 @@
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        message: "What is your Project Title?",
+        name: "projectTitle"
+},
+
+{
+    type: "input",
+    message: "What is your Project Discription?",
+    name: "projectDiscription"
+},
+ {
+    type: "input",
+    message: "What is your Project Installation Instructions?",
+    name: "projectInstallationInstructions "
+},
+
+{
+    type: "input",
+    message: "What is your Project Usage Information?",
+    name: "projectUsageInformation"
+},
+
+{
+    type: "input",
+    message: "What is your Project Contribution Guidelines?",
+    name: "projectContributionGuidelines"
+},
+
+{
+    type: "input",
+    message: "What is your Project Test Instructions?",
+    name:"projectTestInstructions"
+},
+
+{
+    type: "input",
+    message: "What License do you need?",
+    name: "projectLicense",
+    choices:["License 1",  "License2"]
+},
+
+{
+    type: "input",
+    message: "What is your Github user name?",
+    name:"username"
+},
+
+{
+    type: "input",
+    message: "What is your email address?",
+    name:"emailaddress"
+},
+
+{
+    type: "input",
+    message: "Please click on links from Table of contents?",
+    name:"clickOnLinks"
+},
+
+
+{
+    type: "input",
+    message: "What is your Project Test Instructions?",
+    name:"projectTestInstructions"
+},
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+
+    
+
+}
+
+// function to initialize program
+function init() {
+    inquirer.prompt(questions)
+    .then(function(response) {
+        console.log(response);
+});
+
+}
+
+// function call to initialize program
+init();

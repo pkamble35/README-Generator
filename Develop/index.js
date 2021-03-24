439dca4aa156d2b41fc985dbb6dce1c5f56163c6
priyanka damodar <priyadamodar12@gmail.com>
2021-03-24 00:47:01 -0400

    added a mothod to generate readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,71 +1,74 @@
 // array of questions for user
+var inquirer = require("inquirer");
+
+
 const questions = [
     {
         type: "input",
         message: "What is your Project Title?",
         name: "projectTitle"
-},
+    },
 
-{
-    type: "input",
-    message: "What is your Project Discription?",
-    name: "projectDiscription"
-},
- {
-    type: "input",
-    message: "What is your Project Installation Instructions?",
-    name: "projectInstallationInstructions "
-},
+    {
+        type: "input",
+        message: "What is your Project Discription?",
+        name: "projectDiscription"
+    },
+    {
+        type: "input",
+        message: "What is your Project Installation Instructions?",
+        name: "projectInstallationInstructions"
+    },
 
-{
-    type: "input",
-    message: "What is your Project Usage Information?",
-    name: "projectUsageInformation"
-},
+    {
+        type: "input",
+        message: "What is your Project Usage Information?",
+        name: "projectUsageInformation"
+    },
 
-{
-    type: "input",
-    message: "What is your Project Contribution Guidelines?",
-    name: "projectContributionGuidelines"
-},
+    {
+        type: "input",
+        message: "What is your Project Contribution Guidelines?",
+        name: "projectContributionGuidelines"
+    },
 
-{
-    type: "input",
-    message: "What is your Project Test Instructions?",
-    name:"projectTestInstructions"
-},
+    {
+        type: "input",
+        message: "What is your Project Test Instructions?",
+        name: "projectTestInstructions"
+    },
 
-{
-    type: "input",
-    message: "What License do you need for project?",
-    name: "projectLicense",
-    choices:["License 1",  "License2", "License 3", "License 4"]
-},
+    {
+        type: "list",
+        message: "What License do you need?",
+        name: "projectLicense",
+        choices: ["License 1", "License2"]
+    },
 
-{
-    type: "input",
-    message: "What is your Github user name?",
-    name:"username"
-},
+    {
+        type: "input",
+        message: "What is your Github user name?",
+        name: "username"
+    },
 
-{
-    type: "input",
-    message: "What is your email address?",
-    name:"emailaddress"
-},
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "emailaddress"
+    },
 
-{
-    type: "input",
-    message: "click on links from Table of contents?",
-    name:"clickOnLinks"
-},
+    {
+        type: "input",
+        message: "Please click on links from Table of contents?",
+        name: "clickOnLinks"
+    },
 
 
-{
-    type: "input",
-    message: "What is your Project Test Instructions?",
-    name:"projectTestInstructions"
-},
+    {
+        type: "input",
+        message: "What is your Project Test Instructions?",
+        name: "projectTestInstructions"
+    }
 
 ];
 
@@ -76,11 +79,27 @@ function writeToFile(fileName, data) {
 
 }
 
+//function to create Template of Readme
+
+function generateReadme(response) {
+
+    var projectTitle = ( "# "  +  response.projectTitle  + "\n");
+    var projectDiscription = (response.projectDiscription + "\n");
+    var projectInstallation = ("## Installation" + "\n" + response.projectInstallationInstructions + "\n");
+    console.log(projectTitle + projectDiscription + projectInstallation);
+
+
+
+}
+
 // function to initialize program
 function init() {
     inquirer.prompt(questions)
     .then(function(response) {
-        console.log(response);
+
+
+        generateReadme(response);
+        
 });
 
 }
